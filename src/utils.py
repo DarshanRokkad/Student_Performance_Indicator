9e96d49bb7272d6ef4037181ec1280b1871973ff
Darshan R.M <90130734+darshancpu@users.noreply.github.com>
2023-12-29 01:24:16 +0530

    added evaluate model method

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ from src.exception import CustomException
 import numpy as np
 import pandas as pd
 import dill         # used to pickle the model or preprocessors
+from sklearn.metrics import r2_score
+
 
 def save_object(file_path, obj):
     try:
@@ -13,5 +15,35 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok = True)
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
+        
+        logging.info('object saved successfully')
+    
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_models(x_train, x_test, y_train, y_test, models):
+    try:
+
+        logging.info('Model evaluation starts')
+        
+        report = {}    
+
+        for i in range(len(list(models))):
+            # model creation
+            regressor = list(models.values())[i]
+            # model training
+            regressor.fit(x_train, y_train)
+            # prediction
+            y_pred = regressor.predict(x_test)
+            # model score 
+            r2 = r2_score(y_test, y_pred)
+            # adding score to report
+            report[list(models.keys())[i]] = r2
+
+        logging.info('Model evaluation complete')
+        
+        return report
+    
+    except Exception as e:
+        CustomException(e, sys)
