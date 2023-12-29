5ed1cb3e587e588e51aa0c35106d2ee1eaaf79ec
Darshan R.M <90130734+darshancpu@users.noreply.github.com>
2023-12-29 17:57:01 +0530

    added api for testing using postman

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import numpy as np 
 import pandas as pd 
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
@@ -10,6 +10,30 @@ app = application
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# API for doing api testing using postman 
+@app.route('/predict_api', methods = ['GET', 'POST'])
+def predict_output_api():
+    if request.method == 'GET':
+        return 0
+    else:
+        # getting data from postman api 
+        data = CustomData(
+            gender = request.json['gender'],
+            race_ethnicity = request.json['race_ethnicity'],
+            parental_level_of_education = request.json['parental_level_of_education'],
+            lunch = request.json['lunch'],
+            test_preparation_course = request.json['test_preparation_course'],
+            reading_score = request.json['reading_score'],
+            writing_score = request.json['writing_score']
+        )
+        input_df = data.get_data_as_dataframe()
+        # prediction 
+        predict_pipeline = PredictPipeline()
+        output = predict_pipeline.predict(input_df)
+        result = int(output[0])
+
+        return jsonify(result)
 
 @app.route('/predict', methods = ['GET', 'POST'])
 def predict_output():
